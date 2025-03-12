3904acdccdef977677ffb9a0044000199dc64a89
ColinCMcC <118211798+ColinCMcC@users.noreply.github.com>
2025-03-12 00:56:39 -0500

    1.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Basics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Basics.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class Motor_Basics extends LinearOpMode {
+
+    DcMotor motor = new DcMotor();
+
     @Override
     public void runOpMode() {
 
