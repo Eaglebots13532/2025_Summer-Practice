44cd3601fe39acef09043b199acd3fd0d04096cc
ColinCMcC <118211798+ColinCMcC@users.noreply.github.com>
2025-03-12 01:58:49 -0500

    3/11/2025
    Error13532

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Basics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor_Basics.java
@@ -1,13 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Config
+@TeleOp
 public class Motor_Basics extends LinearOpMode {
 
-    DcMotor motor = new DcMotor();
 
-    @Override
-    public void runOpMode() {
+  FtcDashboard dashboard = FtcDashboard.getInstance();
+
+  DcMotorEx LMotor, RMotor;
+
+  public static double power = 0;
+
+  @Override
+  public void runOpMode() {
+
+    telemetry = dashboard.getTelemetry();
+
+    LMotor = hardwareMap.get(DcMotorEx.class, "BLMotor");
+    RMotor = hardwareMap.get(DcMotorEx.class, "BRMotor");
+
+    LMotor.setDirection(REVERSE);
+
+
+    waitForStart();
+    while (opModeIsActive()) {
+
+      telemetry.addData("AddData", LMotor.getVelocity());
+      telemetry.update();
+
+      RMotor.setPower(power);
+      LMotor.setPower(power);
+
+      telemetry.update();
+
 
     }
+  }
 }
